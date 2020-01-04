bb7033211fe4f77539798f74164c6cdc39b36ca7
Daniel Knüppe <daniel_k@posteo.net>
2020-01-04 04:58:16 +0100

    well now at least

--- software/motor_driver/src/motor_driver.c
+++ software/motor_driver/src/motor_driver.c
@@ -9,23 +9,12 @@ int main()
 {
     wiringPiSetup();
     PWMObservable pwm = newPWMObservable();
-<<<<<<< HEAD
-    pwm->frequency = 1000;
-    pwm->dutyCycle = 20;
-    PWMObserver in1 = newPWMObserver(25, false);
-    PWMObserver in2 = newPWMObserver(26, true);
-    PWMObserver enable = newPWMObserver(28, false);
-    pwm->clazz->observe((Observable)pwm, (void*)in1);
-    pwm->clazz->observe((Observable)pwm, (void*)in2);
-    pwm->clazz->observe((Observable)pwm, (void*)enable);
-=======
     pwm->frequency = 440;
     pwm->dutyCycle = 100;
     PWMObserver in1 = newPWMObserver(3, false);
     PWMObserver in2 = newPWMObserver(5, false);
     PWMObserver enable = newPWMObserver(4, false);
     pwm->clazz->observe((Observable)pwm, (void*)in1);
->>>>>>> d03de3371ecd86c9d0585a59be44f47ef6ad97a7
     pwm->clazz->start(pwm);
     for(unsigned int i = 0;; i+=3){
         pwm->clazz->setDutyCycle(pwm, i%100);
